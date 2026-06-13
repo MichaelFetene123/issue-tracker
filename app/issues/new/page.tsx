13b2b05301f24546e2c37bbf3f1f1a1c20ab5a63
MichaelFetene123 <michaelfetene0202@gmail.com>
2026-06-13 12:07:15 +0300

    add a markdown editor

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,13 +1,17 @@
+"use client"
 import React from 'react'
 import { Input } from "@/components/ui/input"
-import { Textarea } from '@/components/ui/textarea'
 import { Button } from '@/components/ui/button'
+import dynamic from 'next/dynamic'
+
+const SimpleMDE = dynamic(() => import("react-simplemde-editor"), { ssr: false });
+import "easymde/dist/easymde.min.css";
 
 export default function newIssuePage() {
   return (
     <div className='max-w-xl space-y-3'>
       <Input placeholder="Title" />
-      <Textarea placeholder="Description" />
+      <SimpleMDE placeholder="Description" />
       <Button>Submit New Issue</Button>
     </div>
   )
